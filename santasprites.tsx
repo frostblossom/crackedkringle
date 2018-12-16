137f42a84902f523ea20f35ab4e684f91b02aea9
Alex <achythlook@gmail.com>
2018-12-16 10:46:48 -0900

    Add some tiled stuff

---
+++ santasprites.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="santasprites" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="santasprites.png" width="128" height="128"/>
+</tileset>
